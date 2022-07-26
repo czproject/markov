79b3b2bcada77f32e323b1ff2e45ed2b9eaadf14
Jan Pecha <janpecha@email.cz>
2022-07-26 13:13:42 +0200

    Tests: fixed Tests::unindent()

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,13 +20,20 @@ class Tests
 		$s = ltrim($s, "\n\r"); // remove empty lines
 		$s = rtrim($s);
 		$lines = explode("\n", $s);
-		$prefix = \Nette\Utils\Strings::findPrefix(array_filter($lines, function ($value) {
+
+		$linesToSearch = $lines;
+		array_walk($linesToSearch, function (&$value) {
+			$m = \Nette\Utils\Strings::match($value, '~^\s+~');
+			$value = isset($m[0]) ? $m[0] : '';
+		});
+		$linesToSearch = array_filter($linesToSearch, function ($value) {
 			return $value !== '';
-		}));
-		$prefixLength = \Nette\Utils\Strings::length($prefix);
+		});
+
+		$prefix = \Nette\Utils\Strings::findPrefix($linesToSearch);
 
 		foreach ($lines as &$line) {
-			$line = \Nette\Utils\Strings::substring($line, $prefixLength);
+			$line = \Nette\Utils\Strings::after($line, $prefix);
 		}
 
 		return implode("\n", $lines) . "\n";
